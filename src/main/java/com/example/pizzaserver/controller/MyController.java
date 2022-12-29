14cbb4c903eebc953487567778ca7a1e173f2968
Pavel <impetus@yandex.ru>
2022-12-29 20:37:03 +0300

    new comment

--- src/main/java/com/example/pizzaserver/controller/MyController.java
+++ src/main/java/com/example/pizzaserver/controller/MyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+// new comment
 @Controller
 public class MyController {
 
